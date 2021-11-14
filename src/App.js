2e472b440eb994ad6e92ae0c342d063b81c86a4b
Thuong To <thuongthanhto@gmail.com>
2021-11-14 11:47:54 +0700

    Add tailwinds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-     thuong thanh to
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="flex-shrink-0">
+      <img className="h-12 w-12" src="https://www.airdrop.realrealm.io/static/media/logo.80245b99.png" alt="ChitChat Logo" />
     </div>
+    <div>
+      <div className="text-xl font-medium text-black">ChitChat</div>
+      <p className="text-gray-500">You have a new message!</p>
+    </div>
+  </div>
   );
 }
 

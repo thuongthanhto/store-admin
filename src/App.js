e37dabe292bf0566243e5a8861f592f5c5246fb4
Thuong To <thuongthanhto@gmail.com>
2021-11-24 20:38:24 +0700

    Update router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   Link
 } from "react-router-dom";
@@ -11,81 +11,40 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
+        <nav style={{display: 'flex', justifyContent: 'center'}}>
+          <ul style={{display: 'flex'}}>
+            <li style={{padding: 10}}>
               <Link to="/">Home</Link>
             </li>
-            <li>
+            <li style={{padding: 10}}>
               <Link to="/about">About</Link>
             </li>
-            <li>
+            <li style={{padding: 10}}>
               <Link to="/users">Users</Link>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/about" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
 }
 function Home() {
-  return <h2>Home</h2>;
+  return <h2 className="mt-5 text-center">Home</h2>;
 }
 
 function About() {
-  return <h2>About</h2>;
+  return <h2 className="mt-5 text-center">About</h2>;
 }
 
-function Topics() {
-  let match = useRouteMatch();
-
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
+function Users() {
+  return <h2 className="mt-5 text-center">Users</h2>;
 }
 
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
-}
+
 export default App;
